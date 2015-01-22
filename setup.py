e0cdcb75bbd9884eb144aebbf8b51681ebee492a
Joost Molenaar <j.j.molenaar@gmail.com>
2015-01-22 20:38:27 +0100

    Bump version to 1.3.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author='Joost Molenaar',
     author_email='j.j.molenaar@gmail.com',
     url='https://github.com/j0057/setuptools-version-command',
-    version='1.3.3',
+    version='1.3.4',
     description=description,
     long_description=long_description,
     py_modules=['setuptools_version_command'],
