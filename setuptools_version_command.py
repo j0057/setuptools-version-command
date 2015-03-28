4f06fa63d1f9150ebe21dae8fe4c5fa32fc15fe6
Joost Molenaar <j.j.molenaar@gmail.com>
2015-03-28 13:25:35 +0100

    New PEP440 algorithm

--- setuptools_version_command.py
+++ setuptools_version_command.py
@@ -1,12 +1,11 @@
 import os
 import os.path
+import re
 import subprocess
 
-# version_command=('git describe', 'pep440-git-dev')
-# version_command=('git describe', 'pep440-git-local')
-# version_command=('git describe', 'pep440-git')
-# version_command=('git describe', None)
-# version_command='git describe'
+# see pep440-version-regex.png
+_PEP440_VERSION = re.compile(r'^(?P<v>v)?(?:(?P<e>\d+)(?P<e_s>!))?(?P<r>\d+(?:\.\d+)*)(?:(?P<pre_ps>[\._-])?(?P<pre_t>a|alpha|b|beta|rc|c|pre|preview)(?P<pre_is>[\._-](?=\d))?(?P<pre_n>\d*))?(?:(?:(?P<post_ps>[\._-])?(?P<post_t>post|rev|r)(?P<post_is>[\._-](?=\d))?|(?P<post_im>-))(?P<post_n>(?(post_t)\d*|\d+)))?(?:(?P<dev_ps>[\._-])?(?P<dev_t>dev)(?P<dev_is>[\._-](?=\d))?(?P<dev_n>\d*))?(?:-(?P<git_rev>\d+)-(?P<git_commit>g?[0-9a-f]{4,20}))?$', re.IGNORECASE|re.VERBOSE)
+_PEP440_POST_MODE = re.compile(r'^(?:(?P<post_ps>[\._-]?)(?P<post_t>post|rev|r)(?P<post_is>[\._-]?)|(?P<post_im>-))?$')
 
 def validate_version_command_keyword(dist, attr, value):
     egg_info_dir = dist.metadata.name.replace('-', '_') + '.egg-info'
@@ -14,16 +13,19 @@ def validate_version_command_keyword(dist, attr, value):
     version_txt      = egg_info_dir + '/version.txt'
     version_full_txt = egg_info_dir + '/version_full.txt'
 
-    (command, pep440_mode) = _parse_value(value)
+    (command, pep440_mode, pep440_post) = _parse_value(value)
 
-    current_short_version, current_full_version = _get_scm_version(command, pep440_mode)
-    cached_short_version, cached_full_version = _get_cached_version(version_txt, version_full_txt)
+    (current_short_version, current_full_version) = _get_scm_version(command, pep440_mode, pep440_post)
+    (cached_short_version, cached_full_version) = _get_cached_version(version_txt, version_full_txt)
 
-    if not (current_short_version or cached_short_version):
+    if current_short_version:
+        dist.metadata.version = current_short_version
+        dist.metadata.version_full = current_full_version
+    elif cached_short_version:
+        dist.metadata.version = cached_short_version
+        dist.metadata.version_full = cached_full_version
+    else:
         raise Exception('Could not find version from {0!r} or from {1}'.format(command, version_full_txt))
-
-    dist.metadata.version = current_short_version or cached_short_version
-    dist.metadata.version_full = current_full_version or cached_full_version
 
 def write_metadata_value(command, basename, filename):
     attr_name = os.path.splitext(basename)[0]
@@ -34,26 +36,34 @@ def write_metadata_value(command, basename, filename):
 
 def _parse_value(value):
     if isinstance(value, str):
-        return (value, None)
+        value = (value, None)
 
-    elif isinstance(value, tuple) and len(value) == 2:
+    if isinstance(value, tuple) and len(value) == 2:
+        value = value + ('.post',)
 
+    if isinstance(value, tuple) and len(value) == 3:
         if value[0] not in ['git describe']:
-            raise Exception('Unsupported SCM command {0!r}'.format(value))
+            raise Exception('Unsupported SCM command {0!r}'.format(value[0]))
 
         if value[1] is None:
             pass
         elif value[1] in ['pep440-git', 'pep440-git-dev', 'pep440-git-local']:
             pass
         else:
-            raise Exception ('Unrecognized value {0!r}', value[1])
+            raise Exception('Unrecognized PEP440 mode {0!r}'.format(value[1]))
 
-        return value
+        (command, pep440_mode, pep440_post) = value
+        match = _PEP440_POST_MODE.match(pep440_post)
+        if not match:
+            raise Exception('Unrecognized post mode {0!r}'.format(value[2]))
+        pep440_post_mode = { k: v or '' for (k, v) in match.groupdict().items() }
+
+        return (command, pep440_mode, pep440_post_mode)
 
     else:
-        raise Exception('Unrecognized value {0!r}'.format(value))
+        raise Exception('Unrecognized version_command value {0!r}'.format(value))
 
-def _get_scm_version(command, pep440_mode):
+def _get_scm_version(command, pep440_mode, pep440_post):
     try:
         cmd = command.split()
         full_version = subprocess.check_output(cmd).strip()
@@ -61,7 +71,9 @@ def _get_scm_version(command, pep440_mode):
         full_version = None
 
     if full_version:
-        short_version = _apply_pep440(full_version, pep440_mode)
+        short_version = _apply_pep440(full_version, pep440_mode, pep440_post)
+        if not short_version:
+            raise Exception('Could not transform version {0!r}'.format(full_version))
     else:
         short_version = None
 
@@ -77,27 +89,64 @@ def _read_version(filename):
     except:
         return None
 
-def _apply_pep440(version, mode):
+def _split_version(version):
+    match = _PEP440_VERSION.match(version)
+    if not match:
+        raise Exception('Can\'t parse version {0!r} as PEP440 version'.format(version))
+    return { k: v or '' for (k, v) in match.groupdict().items() }
 
-    if mode in ['pep440-git-local']:
+def _join_version(v):
+    return '{v}{e}{e_s}{r}{pre_ps}{pre_t}{pre_is}{pre_n}{post_ps}{post_t}{post_is}{post_im}{post_n}{dev_ps}{dev_t}{dev_is}{dev_n}'.format(**v)
+
+def _apply_pep440(version, pep440_mode, pep440_post={'post_ps':'.', 'post_t': 'post', 'post_is': '', 'post_im': ''}):
+    if pep440_mode in ['pep440-git-local']:
         return version.replace('-', '+git-', 1).replace('-', '.')
 
-    elif mode in ['pep440-git', 'pep440-git-dev']:
-        if version.endswith('.dev'):
-            return version + '0'  # on tag
-        elif '.dev-' in version:
-            parts = version.split('-')
-            return parts[0] + parts[1]
-        elif '-' in version:
-            # XXX: This in not compliant with PEP440. It is supported here for backwards-compatibility
+    elif pep440_mode in ['pep440-git-dev']:
+        # XXX: This is not compliant with PEP440. It is supported here for backwards-compatibility
+        if '-' in version:
             parts = version.split('-')
             parts[-2] = 'dev' + parts[-2]
             return '.'.join(parts[:-1])
         else:
             return version
 
-    elif mode is None:
+    elif pep440_mode in ['pep440-git']:
+        vd = _split_version(version)
+        revs = vd['git_rev'] or '0'
+
+        # on git tag, so don't do anything
+        if revs == 'x':
+            pass
+        # has dev tag, update number if it's implicitly 0
+        elif vd['dev_t']:
+            if vd['dev_n'] == '':
+                vd['dev_n'] = revs
+            elif revs != '0':
+                return None
+        # has post tag, update number if it's implicitly 0
+        elif vd['post_t'] or vd['post_im']:
+            if vd['post_n'] == '':
+                vd['post_n'] = revs
+            elif revs != '0':
+                return None
+        else:
+            # update pre tag if it's implicitly 0
+            if vd['pre_t'] and (vd['pre_n'] == ''):
+                vd['pre_n'] = revs
+            # else add a post tag
+            elif revs != '0':
+                vd.update(pep440_post)
+                vd['post_n'] = revs
+
+        if vd['pre_t'] and not vd['pre_n']: vd['pre_n'] = '0'
+        if (vd['post_t'] or vd['post_im']) and not vd['post_n']: vd['post_n'] = '0'
+        if vd['dev_t'] and not vd['dev_n']: vd['dev_n'] = '0'
+
+        return _join_version(vd)
+
+    elif pep440_mode is None:
         return version
 
     else:
-        raise Exception('Unrecognized mode {0!r}'.format(mode))
+        raise Exception('Unrecognized PEP440 mode {0!r}'.format(pep440_mode))
